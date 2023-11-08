650019698b5d8963ffe4db3e88d63e88a8ab1e63
Iamagenus <brandonlewandowski@icloud.com>
2023-11-08 16:05:59 -0500

    color updatd and new logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Soumyajit Behera and Rebuilt by iamagenus</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} IAG</h3>
+          <h3>Copyright © {year} IG</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
