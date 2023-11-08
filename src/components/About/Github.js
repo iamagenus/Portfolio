ee1f8352d7be25601813e6336c98522cbbf64ddf
Iamagenus <brandonlewandowski@icloud.com>
2023-11-08 12:50:51 -0500

    replaced sb with Iamgenus in index github and foot

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -9,10 +9,10 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="soumyajit4419"
+        username="iamagenus"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#19503e"
         fontSize={16}
       />
     </Row>
